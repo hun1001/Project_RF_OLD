a4808b00b59224de7fcd407b9fd15bd9063c6fab
hun1001 <l01022899266@gmail.com>
2023-01-07 18:11:31 +0900

    조이스틱 완성

---
+++ Assets/Scripts/UI/JoyStick/JoyStick.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    public class JoyStick : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
+    {
+        private RectTransform _rectTransform = null;
+        private RectTransform _rectTransformChild = null;
+        
+        private Vector2 _direction = Vector2.zero;
+        
+        private float _radius = 0.0f;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _rectTransformChild = transform.GetChild(0).GetComponent<RectTransform>();
+            _radius = _rectTransform.rect.width * 0.5f;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _rectTransformChild.position = eventData.position;
+        }
+        
+        public void OnDrag(PointerEventData eventData)
+        {
+            Vector2 pos = eventData.position - (Vector2)_rectTransform.position;
+            pos = Vector2.ClampMagnitude(pos, _radius);
+            _rectTransformChild.localPosition = pos;
+            
+            _direction = pos.normalized;
+            
+            Debug.Log(_direction);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _rectTransformChild.localPosition = Vector2.zero;
+        }
+
+        public Vector2 Direction => _direction;
+        public  float Vertical => _direction.y;
+        public float Horizontal => _direction.x;
+    }
+}
