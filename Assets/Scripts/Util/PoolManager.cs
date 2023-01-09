60e06293c6242d443da1036d0116287ccd920227
hun1001 <l01022899266@gmail.com>
2023-01-09 18:49:00 +0900

    풀메니져 제작 중

--- Assets/Scripts/Util/PoolManager.cs
+++ Assets/Scripts/Util/PoolManager.cs
@@ -1,11 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Util;
+using UnityEngine.AddressableAssets;
 
-namespace Util
+namespace Pool
 {
     public class PoolManager : Singleton<PoolManager>
     {
-        private Dictionary<string, Queue<GameObject>> _objectDictionaryQueue = null;
+        private Dictionary<string, Queue<GameObject>> _poolingDictionaryQueue = null;
+
+        /// <summary>
+        /// 오브젝트 가져오기
+        /// </summary>
+        /// <param name="address"></param>
+        public GameObject Get(string address)
+        {
+            GameObject temp = null;
+
+            if (_poolingDictionaryQueue.ContainsKey(address))
+            {
+                if (_poolingDictionaryQueue[address].Count > 0)
+                {
+                    temp = _poolingDictionaryQueue[address].Dequeue();
+                }
+                else
+                {
+                }
+            }
+            else
+            {
+
+            }
+
+            return temp;
+        }
+
+        public void PoolObject(string address)
+        {
+
+        }
     }
 }
