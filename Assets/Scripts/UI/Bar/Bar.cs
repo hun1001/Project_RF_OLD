f8798d99c82453c8f114e50278b26180026683ec
hun1001 <l01022899266@gmail.com>
2023-01-09 16:36:21 +0900

    bar 임시 코드 제작
    리소스 받으면 조정 예정

--- Assets/Scripts/UI/Bar/Bar.cs
+++ Assets/Scripts/UI/Bar/Bar.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class Bar : MonoBehaviour
     {
+        private float _maxValue;
+        private float _value;
+
+        private Image _barImage = null;
+
+        private void SetValue()
+        {
+            _barImage.fillAmount = _value / _maxValue;
+        }
 
     }
 }
