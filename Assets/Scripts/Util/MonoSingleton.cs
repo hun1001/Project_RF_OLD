612121e0c67ca24f18d197ff90f1cc96741fb651
hun1001 <l01022899266@gmail.com>
2023-01-09 17:52:40 +0900

    싱글톤 추가

--- Assets/Scripts/Util/MonoSingleton.cs
+++ Assets/Scripts/Util/MonoSingleton.cs
@@ -1,11 +1,36 @@
+using System.Globalization;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Util
 {
     public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
     {
+        private static readonly Lazy<T> _instance =
+            new Lazy<T>(() =>
+            {
+                T instance = FindObjectOfType(typeof(T)) as T;
 
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).FullName);
+                    instance = obj.AddComponent(typeof(T)) as T;
+
+                    DontDestroyOnLoad(obj);
+                }
+                DontDestroyOnLoad(instance);
+
+                return instance;
+            });
+
+        public static T Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
     }
 }
