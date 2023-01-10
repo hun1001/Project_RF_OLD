79c56b110a4002543ab8e55cf4eb94b3864192e1
hun1001 <l01022899266@gmail.com>
2023-01-10 18:25:26 +0900

    Update TankSO.cs

--- Assets/Scripts/SO/TankSO.cs
+++ Assets/Scripts/SO/TankSO.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SO/Data/Tank")]
 public class TankSO : ScriptableObject
 {
-    [Header("Tank's Hp")]
+    [Header("Hp")]
     [Range(0f, 10000f)]
     public float hp;
-    [Header("Tank's Speed")]
+    [Header("Speed")]
     [Range(0f, 10f)]
     public float speed;
-    [Header("Tank's Reload Speed")]
+    [Header("Reload Speed")]
     [Range(0f, 20f)]
     public float reloadSpeed;
 }
