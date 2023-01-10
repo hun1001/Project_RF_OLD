ea6653db7e5cb9738a31b64c2e017a6534a3c248
hun1001 <l01022899266@gmail.com>
2023-01-10 22:54:23 +0900

    Update Camera_Move.cs

--- Assets/Scripts/Camera/Base/Component/Camera_Move.cs
+++ Assets/Scripts/Camera/Base/Component/Camera_Move.cs
@@ -23,8 +23,6 @@ namespace CameraManage
         {
             Vector3 moveDir = new Vector3(_joyStick.Direction.x, 0, _joyStick.Direction.y);
 
-            Debug.Log(moveDir);
-
             transform.position += moveDir * Time.deltaTime * 10;
         }
     }
