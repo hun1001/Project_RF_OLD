588bad1ae271f47b8ff039db0c64517cc8809af1
hun1001 <l01022899266@gmail.com>
2023-01-11 19:56:22 +0900

    포탄 풀 적용

---
+++ Assets/Scripts/Shell/Shell_Pool.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+namespace Shell
+{
+    public class Shell_Pool : ShellComponent
+    {
+        private float _lifeTime = 1f;
+
+        private void OnEnable()
+        {
+            _lifeTime = Instance.Range / Instance.Speed;
+            StartCoroutine(DisableShell());
+        }
+
+        private IEnumerator DisableShell()
+        {
+            yield return new WaitForSeconds(_lifeTime);
+            PoolManager.Instance.Pool("Assets/Prefabs/Shell/Shell.prefab", this.gameObject);
+        }
+    }
+}
