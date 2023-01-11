728371d5cffaae641a75722639f64ab61fb33906
hun1001 <l01022899266@gmail.com>
2023-01-11 20:03:09 +0900

    so 위치 변경 데미지 적용

--- Assets/Scripts/Shell/Shell_Collision.cs
+++ Assets/Scripts/Shell/Shell_Collision.cs
@@ -14,6 +14,7 @@ namespace Shell
                 case HitType.PENETRATION:
                     PoolManager.Instance.Get("Assets/Prefabs/Effect/WFX_ExplosiveSmoke.prefab", Instance.transform.position);
                     PoolManager.Instance.Pool("Assets/Prefabs/Shell/Shell.prefab", gameObject);
+                    other.gameObject.SendMessage("OnHit", 250);
                     break;
                 case HitType.RICOCHET:
                     transform.rotation = Quaternion.LookRotation(Vector3Calculator.GetReflectionVector(Instance.transform.forward.normalized, other.contacts[0].normal));
