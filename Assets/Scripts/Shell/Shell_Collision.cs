4f8c3bcf4506b6854239d5bbc0327a87b7e11e98
hun1001 <l01022899266@gmail.com>
2023-01-12 00:16:49 +0900

    무엇이 문제였을까?

--- Assets/Scripts/Shell/Shell_Collision.cs
+++ Assets/Scripts/Shell/Shell_Collision.cs
@@ -11,16 +11,16 @@ namespace Shell
         {
             float angle = Vector3Calculator.GetIncomingAngle(Instance.transform.forward.normalized, other.contacts[0].normal);
 
+            Debug.Log(angle);
+
             switch (TypeReader.GetHitType(angle))
             {
                 case HitType.PENETRATION:
-                    Debug.Log("Penetration");
                     PoolManager.Instance.Get("Assets/Prefabs/Effect/WFX_ExplosiveSmoke.prefab", Instance.transform.position);
                     PoolManager.Instance.Pool("Assets/Prefabs/Shell/Shell.prefab", gameObject);
                     other.gameObject.SendMessage("OnHit", 250, SendMessageOptions.DontRequireReceiver);
                     break;
                 case HitType.RICOCHET:
-                    Debug.Log("Ricochet");
                     transform.rotation = Quaternion.LookRotation(Vector3Calculator.GetReflectionVector(Instance.transform.forward.normalized, other.contacts[0].normal));
                     break;
             }
