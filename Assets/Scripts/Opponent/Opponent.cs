2f17efd4ef6132721395fbb517a3f3d3df496ac4
hun1001 <l01022899266@gmail.com>
2023-01-15 11:22:39 +0900

    스크립트 설정

---
+++ Assets/Scripts/Opponent/Opponent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Base;
+
+namespace Opponent
+{
+    public class Opponent : CustomGameObject
+    {
+
+    }
+}
--- Assets/Scripts/Player/Player_Generator.cs
+++ Assets/Scripts/Player/Player_Generator.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UI;
 using Util;
 
-public class Player_Generator : Base.CustomComponent<Player.Player>
+namespace Player
 {
-    private void Awake()
+    public class Player_Generator : PlayerComponent
     {
-        GameObject tank = PoolManager.Instance.Get("Assets/Prefabs/Tanks/Tank_Tiger.prefab", Vector3.zero, Quaternion.identity);
-        tank.GetComponent<Tank.Tank>().Assignment(new Tank.TankUserData(Instance.MoveJoyStick, Instance.HpBar));
-        tank.GetComponent<Turret.Turret>().Assignment(new Turret.TurretUserData(Instance.AttackJoyStick, Instance.AttackImage, Instance.AttackCancel));
+        private void Awake()
+        {
+            GameObject tank = PoolManager.Instance.Get("Assets/Prefabs/Tanks/Tank_Tiger.prefab", Vector3.zero, Quaternion.identity);
+            tank.GetComponent<Tank.Tank>().Assignment(new Tank.TankUserData(Instance.MoveJoyStick, Instance.HpBar));
+            tank.GetComponent<Turret.Turret>().Assignment(new Turret.TurretUserData(Instance.AttackJoyStick, Instance.AttackImage, Instance.AttackCancel));
+        }
     }
 }
