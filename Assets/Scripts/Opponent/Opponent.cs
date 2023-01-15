3f9e55ca4025e83c7be469a247f5a7f7c7823e50
hun1001 <l01022899266@gmail.com>
2023-01-15 22:52:13 +0900

    Update Opponent.cs

--- Assets/Scripts/Opponent/Opponent.cs
+++ Assets/Scripts/Opponent/Opponent.cs
@@ -14,6 +14,8 @@ namespace Opponent
         public Transform[] SpawnPoints => _spawnPointParent.GetComponentsInChildren<Transform>().Where(x => x != _spawnPointParent).ToArray();
         public Transform GetRandomSpawnPoint => SpawnPoints[Random.Range(0, SpawnPoints.Length)];
 
-
+        [SerializeField]
+        private SO.OpponentSO _opponentSO = null;
+        public SO.OpponentSO OpponentSO => _opponentSO;
     }
 }
