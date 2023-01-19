1c4cf735d52f1501630522d4262c53d4e5092d9f
hun1001 <l01022899266@gmail.com>
2023-01-19 20:04:59 +0900

    탱크 이동(테스트 안해봄)

--- Assets/Scripts/Tank/AI/TankAI_Move.cs
+++ Assets/Scripts/Tank/AI/TankAI_Move.cs
@@ -2,19 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SO;
+using UnityEngine.AI;
 
 namespace Tank
 {
     public class TankAI_Move : Tank_Move
     {
+        private NavMeshAgent _agent = null;
+        private Transform _target = null;
+
+        private float _range = 10f;
+
+        private bool _isMove = false;
+        
         protected override void Assignment()
         {
             base.Assignment();
+            _agent = GetComponent<NavMeshAgent>();
+            _target = GameObject.Find("Player").transform;
+            
         }
 
         protected override void Update()
         {
+            if (Vector3.Distance(transform.position, _target.position) > _range)
+            {
+                FindMovePoint();
+            }
+        }
 
+        private void FindMovePoint()
+        {
+            Vector3 movePoint = _target.position - transform.position;
+            movePoint = movePoint.normalized * Random.Range(5f, _range);
+
+            NavMeshPath path = new NavMeshPath();
+
+            while (!_agent.CalculatePath(movePoint, path))
+            {
+                movePoint = movePoint.normalized * Random.Range(5f, _range);
+            }
+            
+            _agent.SetDestination(movePoint);
         }
     }
 }
