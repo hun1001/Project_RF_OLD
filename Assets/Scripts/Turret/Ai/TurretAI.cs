15e8f604e4a6f8070b9eb3498b3e01cbb866b901
hun1001 <l01022899266@gmail.com>
2023-01-20 14:52:11 +0900

    바라보는거 임시로 제작

---
+++ Assets/Scripts/Turret/Ai/TurretAI.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Turret
+{
+    public class TurretAI : MonoBehaviour
+    {
+        [SerializeField] 
+        private Transform _turret = null;
+        
+        private Transform _target = null;
+
+        private void Update()
+        {
+            LookTarget();
+        }
+
+        private void LookTarget()
+        {
+            if (_target is null)
+            {
+                return;
+            }
+            
+            Vector2 direction = _target.position - _turret.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            _turret.rotation = Quaternion.Slerp(_turret.rotation, rotation, 1);
+        }
+    }
+}
