221a9692cd956a1ff06d73c946527f50ca76f7a0
hun1001 <l01022899266@gmail.com>
2023-02-10 18:44:24 +0900

    Update Tank_Skill.cs

--- Assets/Scripts/Tank/Component/Tank_Skill.cs
+++ Assets/Scripts/Tank/Component/Tank_Skill.cs
@@ -27,35 +27,7 @@ namespace Tank
                 _isSkill = false;
                 if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
                 {
-                //     IEnumerator exC()
-                //     {
-                //         float[] missileTime = new float[3];
-                //
-                //         for (int i = 0; i < 3; i++)
-                //         {
-                //             missileTime[i] = Random.Range(0.5f, 10f);
-                //         }
-                //         
-                //         for (int i = 0; i < 3; i++)
-                //         {
-                //             for (int j = 0; j < 3; j++)
-                //             {
-                //                 if (missileTime[i] < missileTime[j])
-                //                 {
-                //                     (missileTime[i], missileTime[j]) = (missileTime[j], missileTime[i]);
-                //                 }
-                //             }
-                //         }
-                //         
-                //         for (int i = 0; i < 3; i++)
-                //         {
-                //             yield return new WaitForSeconds(missileTime[0]);
-                //             Debug.Log($"Missile {i} to {hit.point}");
-                            PoolManager.Instance.Get("Assets/Prefabs/Effect/WFXMR_ExplosiveSmokeGround Big.prefab", hit.point + Vector3.up, Quaternion.identity);
-                    //     }
-                    // }
-
-                   // StartCoroutine(exC());
+                    PoolManager.Instance.Get("Assets/Prefabs/Effect/WFXMR_ExplosiveSmokeGround Big.prefab", hit.point + Vector3.up, Quaternion.identity);
                 }
             }
         }
