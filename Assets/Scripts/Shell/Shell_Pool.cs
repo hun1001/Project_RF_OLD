e7a8406eca284cf85e027766ecdf4a0c075ba82c
hun1001 <l01022899266@gmail.com>
2023-02-14 17:34:59 +0900

    최적화 ing

--- Assets/Scripts/Shell/Shell_Pool.cs
+++ Assets/Scripts/Shell/Shell_Pool.cs
@@ -18,7 +18,6 @@ namespace Shell
         private IEnumerator DisableShell()
         {
             yield return new WaitForSeconds(_lifeTime);
-            GetComponent<TrailRenderer>().Clear();
             PoolManager.Instance.Pool("Assets/Prefabs/Shell/Shell.prefab", this.gameObject);
         }
     }
