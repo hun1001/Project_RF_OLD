234e9b4832f4a8cc900c0a0329c78ff4b19da7ca
seomin1210 <seoyun0700@daum.net>
2023-02-22 18:31:26 +0900

    아이템 UI 수정, 아이템 선택시 재화 소모, 아이템 여러개 선택 가능

--- Assets/Scripts/Item/ItemManager.cs
+++ Assets/Scripts/Item/ItemManager.cs
@@ -1,8 +1,9 @@
-using SO;
+﻿using SO;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UI;
+using Keyword;
 
 namespace Item
 {
@@ -11,14 +12,17 @@ namespace Item
         // TODO: Must change this structure
         public GameObject selectObject;
         public ItemBundleSO Items;
-        
+
+        private int _itemSelectCnt = 0;
+
         public void ItemShow()
         {
             Time.timeScale = 0;
-
+            _itemSelectCnt = 0;
             for (int i = 0; i < 3; i++)
             {
                 var setItem = selectObject.transform.GetChild(i).gameObject;
+                setItem.SetActive(true);
                 SetItem(GetRandomItem(), setItem);
             }
         }
@@ -46,14 +50,28 @@ namespace Item
             
             entry.callback.AddListener((data) =>
             {
+                if(PlayerPrefs.GetInt("Gold") < item.itemSO.NecessaryGold)
+                {
+                    //TODO: 재화 부족 창 띄우기
+                    return;
+                }
+                PlayerPrefs.SetInt("Gold", PlayerPrefs.GetInt("Gold") - item.itemSO.NecessaryGold);
+                gameObject.SendMessage("UpdateGoldText");
                 item.AddItem();
-                Time.timeScale = 1;
+                setItem.SetActive(false);
+
                 //TODO: Will change this code
-                var temp = CanvasManager.Instance.GetSceneCanvases(1);
-                var temp2 = temp as GameSceneCanvases;
-                temp2?.ChangeCanvas(0);
+
+                //var temp = CanvasManager.Instance.GetSceneCanvases(1);
+                //var temp2 = temp as GameSceneCanvases;
+                //temp2?.ChangeCanvas(0);
+                if (_itemSelectCnt++ > 1)
+                {
+                    gameObject.SendMessage("BackCanvas");
+                }
             });
-            
+
+            e.triggers.Clear();
             e.triggers.Add(entry);
         }
     }
--- Assets/Scripts/UI/Canvas/GameScene/Canvas/ItemCanvas.cs
+++ Assets/Scripts/UI/Canvas/GameScene/Canvas/ItemCanvas.cs
@@ -4,19 +4,53 @@ using System.Collections.Generic;
 using PlasticPipe.PlasticProtocol.Messages;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using Keyword;
 
 namespace UI
 {
     public class ItemCanvas : BaseCanvas
     {
+        [SerializeField]
+        private Button _backButton = null;
+
+        [SerializeField]
+        private TextController _goldText = null;
+        public TextController GoldText => _goldText;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _backButton.onClick.AddListener(BackCanvas);
+            //EventManager.StartListening(EventKeyword.OnUpdateGold, (gold) =>
+            //{
+            //    _goldText.SetText(gold[0].ToString());
+            //});
+        }
+
         protected override void SetOnEnableAction()
         {
+            UpdateGoldText();
             gameObject.SendMessage("ItemShow");
         }
         
         protected override void SetOnDisableAction()
         {
             
+        }
+
+        public void UpdateGoldText()
+        {
+            _goldText.SetText(PlayerPrefs.GetInt("Gold").ToString());
+        }
+
+        public void BackCanvas()
+        {
+            Time.timeScale = 1f;
+            EventManager.TriggerEvent(EventKeyword.OnUpdateGold, PlayerPrefs.GetInt("Gold"));
+            var temp = CanvasManager.Instance.GetSceneCanvases(1);
+            var temp2 = temp as GameSceneCanvases;
+            temp2?.ChangeCanvas(0);
         }
     }
 }
