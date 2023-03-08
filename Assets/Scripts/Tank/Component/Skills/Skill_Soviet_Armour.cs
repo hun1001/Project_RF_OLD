66bc59feb969c41040e4c419570d07da838c0fae
hun1001 <l01022899266@gmail.com>
2023-03-09 00:20:24 +0900

    아머 있는거 적용 끝

--- Assets/Scripts/Tank/Component/Skills/Skill_Soviet_Armour.cs
+++ Assets/Scripts/Tank/Component/Skills/Skill_Soviet_Armour.cs
@@ -16,12 +16,27 @@ namespace Tank
             if (_currentCooldown > 0f) return;
             base.Skill();
 
+            ChangeLayer("AmourTank", transform);
+
             SendMessage("SetArmour", _armour);
+        }
+
+        private void ChangeLayer(string layerName, Transform objTransform)
+        {
+            foreach (Transform t in objTransform)
+            {
+                t.gameObject.layer = LayerMask.NameToLayer(layerName);
+                if (t.childCount > 0)
+                    ChangeLayer(layerName, t);
+            }
         }
 
         protected override void Restoration()
         {
             float dummy = 0f;
+
+            ChangeLayer("Default", transform);
+
             SendMessage("SetArmour", dummy);
         }
     }
